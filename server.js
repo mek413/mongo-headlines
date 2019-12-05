4f70743ec1ff564a8d61d1fb2a4e6193c1a382a4
Marcus Kelly <mek413@gmail.com>
2019-12-05 14:04:14 -0600

    setup my models for the database

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
